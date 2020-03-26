7f4c8986d40698fb012f7bbab6e0c6df6a9326d8
VuraddoVG <7355608jr@gmail.com>
2020-03-26 23:09:29 +0100

    Tutorial task 2

---
+++ Tut2/Tut2/Student.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Xml.Serialization;
+
+namespace TutorialSolution2
+{
+    [Serializable]
+    [XmlType("student")]
+    public class Student
+    {
+        [XmlElement(ElementName = "FirstName")]
+        public string FirstName { get; set; }
+
+        [XmlElement(ElementName = "LastName")] public string LastName { get; set; }
+
+        [XmlElement(ElementName = "IndexNumber")]
+        public string IndexNumber { get; set; }
+
+        [XmlElement(ElementName = "BirthDate")]
+        public string BirthDate { get; set; }
+
+        [XmlElement(ElementName = "E-Mail")] public string Email { get; set; }
+
+        [XmlElement(ElementName = "MotherName")]
+        public string MotherName { get; set; }
+
+        [XmlElement(ElementName = "FatherName")]
+        public string FatherName { get; set; }
+
+        [XmlElement(ElementName = "Studies")] public Studies studies { get; set; }
+    }
+}
